b5f981c4891d85069d5d8c68082a8b887005571f
diablom14 <divyanshuasadeveloper@gmail.com>
2025-05-27 22:25:54 +0530

    Add project 7 on react router

---
+++ 07_reactRouter/src/components/User/User.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+function User () {
+  const {userId} = useParams()
+  return (
+    <div className='bg-gray-500 text-3xl text-white'>User :{userId} </div>
+  )
+}
+
+export default User 
